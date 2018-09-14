71a5a4e347ffee5be354718fbccf3d5e388957a0
Dimitry <dimitryj21@gmail.com>
2018-09-14 00:21:18 -0400

    Update gradle and glide

--- app/src/main/java/com/djac21/fingerprintauth/Adapter/NewsAdapter.java
+++ app/src/main/java/com/djac21/fingerprintauth/Adapter/NewsAdapter.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.djac21.fingerprintauth.CustomTabs.CustomTabActivityHelper;
 import com.djac21.fingerprintauth.CustomTabs.WebViewActivity;
 import com.djac21.fingerprintauth.Models.NewsModel;
@@ -65,7 +66,9 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         holder.date.setText(prettyTime.format(new Date(time)));
         Glide.with(context)
                 .load(news.get(position).getImage())
-                .placeholder(R.drawable.ic_loading)
+                .apply(new RequestOptions()
+                        .placeholder(R.drawable.ic_loading)
+                        .error(R.drawable.ic_loading))
                 .into(holder.image);
     }
 
